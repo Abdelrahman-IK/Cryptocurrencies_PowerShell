fb349869f4ca383ab3aadd920a7e5029d437b934
Abdelrahman Ikram <32500069+Abdelrahman-IK@users.noreply.github.com>
2017-12-14 01:07:54 +0200

    Add files via upload

---
+++ Crypto.ps1
@@ -0,0 +1,2 @@
+﻿$json = Invoke-WebRequest -Uri https://api.coinmarketcap.com/v1/ticker/?limit=10 | ConvertFrom-Json | Select-Object
+$json |  Select-Object  @{Name="Symbol";Expression={$_.symbol}},@{Name="Coin Name" ;Expression={$_.name}},@{Name="Price/USD";Expression={$_.price_usd}},@{Name="Change Last 24h";Expression={$_.percent_change_24h}}
